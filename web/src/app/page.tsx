7638a6ba6434bb0e187824479e7e283f43c7b210
kodaimura <kodaimura123@gmail.com>
2025-05-03 23:09:32 +0900

    restructure web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,12 +1,25 @@
 import styles from "./page.module.css";
+import Header from '@/app/components/layout/header';
+import Main from '@/app/components/layout/main';
+import Footer from "@/app/components/layout/footer";
+import LoginLink from "@/app/components/parts/button/login-link";
+import SignupLink from "@/app/components/parts/button/signup-link";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <section className={styles.intro}>
-        <h2>Getting Started</h2>
-        <p>Next.js makes building web applications easy and efficient.</p>
-      </section>
-    </div>
+    <>
+      <Header>
+        <LoginLink /><SignupLink />
+      </Header>
+      <Main>
+        <div className={styles.container}>
+          <section className={styles.intro}>
+            <h2>Getting Started</h2>
+            <p>Next.js makes building web applications easy and efficient.</p>
+          </section>
+        </div>
+      </Main>
+      <Footer />
+    </>
   );
 }
