3ad0396e4cc0ded5205c7e7a3d8ac316a38dd45c
kodaimura <kodaimura123@gmail.com>
2025-05-04 23:13:46 +0900

    add (app) questions page

---
+++ web/src/app/(app)/questions/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { api } from '@/lib/api/api.server';
+import styles from './page.module.css';
+import QuestionList from './question-list';
+import type { Question } from "@/types/models";
+
+const QuestionsPage: React.FC = async () => {
+  const questions: Question = await api.get('questions');
+
+  return (
+    <div className={styles.container}>
+      <QuestionList questions={questions} />
+    </div >
+  );
+};
+
+export default QuestionsPage;
---
+++ web/src/app/(app)/questions/question-list.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styles from './question-list.module.css';
+import type { Question } from "@/types/models";
+
+type Props = {
+  questions: Question[];
+};
+
+const QuestionList: React.FC<Props> = ({ questions }) => {
+
+  const formatDate = (dateStr: string) => {
+    const date = new Date(dateStr);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  };
+
+  return (
+    <div className={styles.tableContainer}>
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          <tr>
+            <th className={styles.th}>#</th>
+            <th className={styles.th}>タイトル</th>
+            <th className={styles.th}>内容</th>
+            <th className={styles.th}>答え</th>
+            <th className={styles.th}>レベル</th>
+            <th className={styles.th}>更新日</th>
+          </tr>
+        </thead>
+        <tbody className={styles.tbody}>
+          {questions.map((q, i) => (
+            <tr key={i} className={styles.tr}>
+              <td className={styles.td}>{q.question_id}</td>
+              <td className={styles.td}>{q.question_title}</td>
+              <td className={styles.td}>{q.question_content}</td>
+              <td className={styles.td}>{q.question_answer}</td>
+              <td className={styles.td}>{q.question_level}</td>
+              <td className={styles.td}>{formatDate(q.updated_at)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div >
+  );
+};
+
+export default QuestionList;
